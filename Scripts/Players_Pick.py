4bccc3d7a89f26b818db1a14c67999df5b51249e
Ziad Nader <ziad.nader88@gmail.com>
2020-10-01 11:59:20 +0300

    Initial commit

---
+++ Scripts/Players_Pick.py
@@ -0,0 +1,108 @@
+list_players = []
+total_price = 0
+team_count = []
+player_points = 0
+position = []
+star_player = 0
+i=0
+cost_players = []
+manchester_factor = 0
+
+while ((total_price < 1000)  & (i <sorted_full_data.shape[0])):
+    #for i in tqdm(range(FPL_data.shape[0])):
+    if (sorted_full_data.status[i]=='a'):
+        if (sorted_full_data.iloc[i].web_name in list_players):
+            i = i+1
+            continue
+        if (sorted_full_data.web_name[i]=='Lundstram'):
+            i = i+1
+            continue
+        if (position.count(1) < 2 ) & (sorted_full_data.element_type[i] == 1):
+            position.append(sorted_full_data.element_type[i])
+            list_players.append(sorted_full_data.iloc[i].web_name)
+            total_price += sorted_full_data.now_cost[i]
+            cost_players.append(sorted_full_data.now_cost[i])
+            if (sorted_full_data.iloc[i].web_name in star_players):
+                star_player +=1
+            if ((sorted_full_data.iloc[i].short_name == 'MUN') or 
+                (sorted_full_data.iloc[i].short_name == 'MCI') or 
+                (sorted_full_data.iloc[i].short_name == 'AVL') or 
+                (sorted_full_data.iloc[i].short_name == 'BUR')):
+                manchester_factor += 1
+        elif ((position.count(2) < 5 ) & (sorted_full_data.element_type[i] == 2) 
+             & (sorted_full_data.iloc[i].web_name not in star_players)):
+            position.append(sorted_full_data.element_type[i])
+            list_players.append(sorted_full_data.iloc[i].web_name)
+            total_price += sorted_full_data.now_cost[i]
+            cost_players.append(sorted_full_data.now_cost[i])
+            if (sorted_full_data.iloc[i].web_name in star_players):
+                star_player +=1
+            if ((sorted_full_data.iloc[i].short_name == 'MUN') or 
+                (sorted_full_data.iloc[i].short_name == 'MCI') or 
+                (sorted_full_data.iloc[i].short_name == 'AVL') or 
+                (sorted_full_data.iloc[i].short_name == 'BUR')):
+                manchester_factor += 1
+            #if (sorted_full_data.iloc[i].web_name in star_players):
+                #star_player +=1
+        elif (position.count(3) < 5 ) & (sorted_full_data.element_type[i] == 3):
+            position.append(sorted_full_data.element_type[i])
+            list_players.append(sorted_full_data.iloc[i].web_name)
+            total_price += sorted_full_data.now_cost[i]
+            cost_players.append(sorted_full_data.now_cost[i])
+            if (sorted_full_data.iloc[i].web_name in star_players):
+                star_player +=1
+            if ((sorted_full_data.iloc[i].short_name == 'MUN') or 
+                (sorted_full_data.iloc[i].short_name == 'MCI') or 
+                (sorted_full_data.iloc[i].short_name == 'AVL') or 
+                (sorted_full_data.iloc[i].short_name == 'BUR')):
+                manchester_factor += 1
+        elif (position.count(4) < 3 ) & (sorted_full_data.element_type[i] == 4):
+            position.append(sorted_full_data.element_type[i])
+            list_players.append(sorted_full_data.iloc[i].web_name)
+            total_price += sorted_full_data.now_cost[i]
+            cost_players.append(sorted_full_data.now_cost[i])
+            if (sorted_full_data.iloc[i].web_name in star_players):
+                star_player +=1
+            if ((sorted_full_data.iloc[i].short_name == 'MUN') or 
+                (sorted_full_data.iloc[i].short_name == 'MCI') or 
+                (sorted_full_data.iloc[i].short_name == 'AVL') or 
+                (sorted_full_data.iloc[i].short_name == 'BUR')):
+                manchester_factor += 1
+        #total_price += sorted_full_data.now_cost[i]
+        #print(total_price)
+        #if (sorted_full_data.iloc[i].web_name in star_players):
+            #star_player +=1
+        #print(list_players)
+        if (star_player > 5 ):
+            del list_players[-1]
+            del position[-1]
+            del cost_players[-1]
+            total_price = total_price - sorted_full_data.now_cost[i]
+            star_player = star_player - 1
+
+        if (manchester_factor > 4 ):
+            del list_players[-1]
+            del position[-1]
+            del cost_players[-1]
+            total_price = total_price - sorted_full_data.now_cost[i]
+            manchester_factor = manchester_factor - 1
+
+
+        if total_price > 1000:
+            del list_players[-1]
+            del position[-1]
+            del cost_players[-1]
+            total_price = total_price - sorted_full_data.now_cost[i]
+        elif len(list_players) == 15:
+            break
+        elif ((len(list_players) < 15 ) & (i == sorted_full_data.shape[0] -1)):
+            del list_players[-1]
+            del position[-1]
+            del cost_players[-1]
+            total_price = total_price - sorted_full_data.now_cost[i]
+            i = 0
+
+        i = i + 1
+    else:
+        i = i+1
+        continue
---
+++ Scripts/football_data_api.py
@@ -0,0 +1,156 @@
+#%%
+import numpy as np 
+import pandas as pd 
+import requests 
+import http.client
+import json
+
+#%%
+### Fetch the premier league matchday matches for the first round
+link = 'https://api.football-data.org/v2/competitions/PL/matches?matchday=1'
+
+#### API connection and get request
+connection = http.client.HTTPConnection('api.football-data.org')
+headers = { 'X-Auth-Token': 'bd5630f7aa4d4a388b30f130fd3b053e' }
+connection.request('GET', link , None, headers )
+response = json.loads(connection.getresponse().read().decode())
+
+# %%
+
+home_teams = []
+away_teams = []
+status_matches = []
+for i in response["matches"]:
+    home = i['homeTeam']['name']
+    away = i['awayTeam']['name']
+    status = i['status']
+    home_teams.append(home)
+    away_teams.append(away)
+    status_matches.append(status)
+
+# %%
+matchday_dataset = pd.DataFrame({'home': home_teams, 
+             'away':away_teams, 
+             'status':status_matches})
+# %%
+filename = 'matchday1.csv'
+# Save the table of data as a CSV
+matchday_dataset.to_csv(index=False, path_or_buf=filename)
+# %%
+### Improve the file to include all matchdays with dates as well as the
+# round number for the PL matchday 
+
+link_i = 'https://api.football-data.org/v2/competitions/PL/matches?matchday='
+
+home_teams = []
+away_teams = []
+status_matches = []
+
+for i in range(9):
+    headers = { 'X-Auth-Token': 'bd5630f7aa4d4a388b30f130fd3b053e' }
+    #print(link_i+str(i+1))
+    connection.request('GET', link_i+str(i+1), None, headers )
+    response = json.loads(connection.getresponse().read().decode())
+    print(response)
+  
+    for j in response["matches"]:
+        home = j['homeTeam']['name']
+        away = j['awayTeam']['name']
+        status = j['status']
+        home_teams.append(home)
+        away_teams.append(away)
+        status_matches.append(status)
+
+
+#%%
+matchday_dataset_nine_rounds = pd.DataFrame({'home': home_teams, 
+             'away':away_teams, 
+             'status':status_matches})
+
+
+#%%
+#Import teams info
+teams_data = pd.read_csv('../Data/teams.csv', index_col=False)
+
+# %%
+dict_team_names = {'Arsenal': 'Arsenal FC', 
+                   'Aston Villa' : 'Aston Villa FC',
+                   'Brighton' : 'Brighton & Hove Albion FC',
+                   'Burnley' : 'Burnley FC',
+                   'Chelsea' : 'Chelsea FC',
+                   'Crystal Palace' : 'Crystal Palace FC',
+                   'Everton' : 'Everton FC',
+                   'Fulham' : 'Fulham FC',
+                   'Leicester' : 'Leicester City FC',
+                   'Leeds' : 'Leeds United FC',
+                   'Liverpool' : 'Liverpool FC',
+                   'Man City' : 'Manchester City FC',
+                   'Man Utd' : 'Manchester United FC',
+                   'Newcastle' : 'Newcastle United FC',
+                   'Sheffield Utd': 'Sheffield United FC',
+                   'Southampton': 'Southampton FC',
+                   'Spurs': 'Tottenham Hotspur FC',
+                   'West Brom': 'West Bromwich Albion FC',
+                   'West Ham': 'West Ham United FC',
+                   'Wolves': 'Wolverhampton Wanderers FC'	}
+
+
+# %%
+teams_data['name'] = teams_data['name'].map(dict_team_names)
+# %%
+
+home_features = ['name', 'strength_overall_home', 
+                 'strength_attack_home', 'strength_defence_home']
+
+away_features = ['name', 'strength_overall_away', 
+                 'strength_attack_away', 'strength_defence_away']
+
+#%%
+
+match_strength_home = pd.merge(matchday_dataset,teams_data[home_features],
+                               left_on='home', right_on='name')
+
+match_strength_away = pd.merge(matchday_dataset,teams_data[away_features],
+                               left_on='away', right_on='name')
+
+
+# %%
+final_match = pd.merge(match_strength_home, match_strength_away,
+                       on=['home', 'away', 'status'])
+# %%
+final_match['overall_diff'] = abs(final_match['strength_overall_home'] - 
+                                  final_match['strength_overall_away'])
+
+final_match['attack_diff'] = abs(final_match['strength_attack_home'] - 
+                                 final_match['strength_defence_away'])
+
+final_match['defence_diff'] = abs(final_match['strength_defence_home'] - 
+                                  final_match['strength_attack_away'])
+# %%
+##################
+
+## 9 matches datasets
+
+#################
+
+
+match_strength_home = pd.merge(matchday_dataset_nine_rounds,teams_data[home_features],
+                               left_on='home', right_on='name')
+
+match_strength_away = pd.merge(matchday_dataset_nine_rounds,teams_data[away_features],
+                               left_on='away', right_on='name')
+
+
+# %%
+final_match = pd.merge(match_strength_home, match_strength_away,
+                       on=['home', 'away', 'status'])
+# %%
+final_match['overall_diff'] = (final_match['strength_overall_home'] - 
+                                  final_match['strength_overall_away'])
+
+final_match['attack_diff'] = (final_match['strength_attack_home'] - 
+                                 final_match['strength_defence_away'])
+
+final_match['defence_diff'] = (final_match['strength_defence_home'] - 
+                                  final_match['strength_attack_away'])
+# %%
