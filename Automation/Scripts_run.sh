741ada8b5d83116271c16e00c9c5c04c4be308cd
Ziad Nader <ziad.nader88@gmail.com>
2020-11-22 15:18:36 +0100

    Automation script to fetch store and pick

---
+++ Automation/Scripts_run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python /Users/ziadNader/Desktop/Personal\ Projects/Fantasy\ Premier\ League/Scripts/Data\ Collection/FPL/Fetch_FPL_data.py
+python /Users/ziadNader/Desktop/Personal\ Projects/Fantasy\ Premier\ League/Scripts/Ibrahim/dbappend_alldates.py
+python /Users/ziadNader/Desktop/Personal\ Projects/Fantasy\ Premier\ League/Scripts/Model/Players_Pick.py
