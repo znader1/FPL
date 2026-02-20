bc832e9c7bfbce59c945dc90b17f63eef00c77e6
Ziad Nader <ziad.nader88@gmail.com>
2026-02-20 15:42:02 +0100

    added 8082

--- api/main.py
+++ api/main.py
@@ -30,6 +30,7 @@ if not cors_origins:
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://127.0.0.1:8082",
     ]
 
 app.add_middleware(
